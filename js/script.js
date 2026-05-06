d768000a1193d80f492880cfe3265ae60c0f684e
DanielGengel <daniel.gengel@outlook.com>
2026-05-06 20:00:48 +0200

    basic structure done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+const gallery = document.getElementById("myPhotoGallery");
+const title = document.getElementById("myPhotoGalleryTitle");
 
+const myImages = [
+    "Spain01.jpeg",
+    "Spain02.jpeg",
+    "Spain03.jpeg",
+    "Spain04.jpeg",
+    "Spain05.jpeg",
+    "Spain06.jpeg",
+    "Spain07.jpeg",
+    "Spain08.jpeg",
+    "Spain09.jpeg",
+    "Spain10.jpeg",
+    "Spain11.jpeg",
+    "Spain12.jpeg",
+    "Spain13.jpeg",
+    "Spain14.jpeg",
+    "Spain15.jpeg",
+    "Spain16.jpeg",
+];
 
-console.log("TEST");
+function showGallery() {
+
+    title.innerHTML = "<h1>My Photo Gallery</h1>";
+    let html = "";
+
+    for (let i = 0; i < myImages.length; i++) {
+        html += `
+            <img src="../assets/images/Spain/${myImages[i]}" alt="Bild">
+        `;
+    }
+
+    
+    gallery.innerHTML = html;
+}
