14cd768e7569e28cb4ad13fe6829495499e5b470
DanielGengel <daniel.gengel@outlook.com>
2026-05-07 09:37:31 +0200

    Lightbox finished and responsive

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const lightboxImageCaption = document.getElementById("lightboxImageCaptionID");
 const lightboxImage = document.getElementById("lightboxImageID");
 const btnPrevImage = document.getElementById("buttonPrevImage");
 const btnNextImage = document.getElementById("buttonNextImage");
+const lightboxImageCounter = document.getElementById("lightboxImageCounterID");
 const btnCloseLightbox = document.getElementById("buttonCloseLightbox");
 
 let indexForLightboxImage = 0;
@@ -30,10 +31,6 @@ const myImages = [
     { imageFileName: "Spain16.jpeg", imageCaption: "Image from Northern Spain" },
 ];
 
-
-
-
-
 function showGallery() {
     photoGalleryTitle.innerHTML = "<h1>My Photo Gallery</h1>";
     photoGallery.innerHTML = "";
@@ -45,10 +42,27 @@ function showGallery() {
     }
 }
 
-// Events zuweisen
+// Add functions to buttons
 btnPrevImage.addEventListener("click", showPrevImage);
 btnNextImage.addEventListener("click", showNextImage);
 btnCloseLightbox.addEventListener("click", closeLightbox);
+
+
+
+// Navigation with arrow keyboard keys and escape to close the Lightbox.
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+        showPrevImage();
+    }
+
+    if (event.key === "ArrowRight") {
+        showNextImage();
+    }
+
+    if (event.key === "Escape") {
+        closeLightbox(); 
+    }
+});
 
 function openLightbox(index) {
     // Which picture was clicked at?
@@ -62,22 +76,36 @@ function openLightbox(index) {
 
 function showNextImage() {
     // console.log(document.getElementById("lightboxImageID"));
-    indexForLightboxImage = (indexForLightboxImage + 1) % myImages.length;
-    imageToShow(indexForLightboxImage);
+    // indexForLightboxImage = (indexForLightboxImage + 1) % myImages.length;
+    // imageToShow(indexForLightboxImage);
+    indexForLightboxImage++;
+    if (indexForLightboxImage >= myImages.length) indexForLightboxImage = 0;
+    imageToShow();
 }
 
 function showPrevImage() {
     // console.log(document.getElementById("lightboxImageID"));
-    indexForLightboxImage = (indexForLightboxImage - 1 + myImages.length) % myImages.length;
-    imageToShow(indexForLightboxImage);
+    // indexForLightboxImage = (indexForLightboxImage - 1 + myImages.length) % myImages.length;
+    // imageToShow(indexForLightboxImage);
+    indexForLightboxImage--;
+    if (indexForLightboxImage < 0) indexForLightboxImage = myImages.length - 1;
+    imageToShow();
 }
 
-function imageToShow(index) {
-    lightboxImage.src = `../assets/images/Spain/${myImages[index].imageFileName}`;
-    lightboxImage.setAttribute("alt", `${myImages[index].imageCaption}`);
-    lightboxImageCaption.textContent = `${myImages[index].imageCaption}`;
+function imageToShow() {
+    lightboxImage.src = `../assets/images/Spain/${myImages[indexForLightboxImage].imageFileName}`;
+    lightboxImage.setAttribute("alt", `${myImages[indexForLightboxImage].imageCaption}`);
+    lightboxImageCaption.textContent = `${myImages[indexForLightboxImage].imageCaption}`;
+    lightboxImageCounter.textContent = `${indexForLightboxImage + 1}/${myImages.length}`;
 }
 
 function closeLightbox() {
     lightbox.style.display = "none";
 }
+
+// Close window when clicked outside lightbox
+lightbox.addEventListener("click", (event) => {
+    if (event.target === lightbox) {
+        closeLightbox();
+    }
+});
